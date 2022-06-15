fe527a58eb3f892e143153604534693dfc55c3bb
Ali sherif <a.sheriffouad@gmail.com>
2022-06-15 09:19:41 +0200

    Adding leetcode problem: 24. Swap Nodes in Pairs

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,45 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pq = dummy;
+        ListNode pn = head;
+
+        /*
+        if there only one node or it is last node and it will not change becouse input( odd ) (pn.next will be null == null) yes 
+
+        Short-circuit AND Operator so it prevent my from second condition be null.next != null
+        */
+        while (pn != null && pn.next != null) {
+
+            pq.next = pn.next;
+            pn.next = pq.next.next;
+            pq.next.next = pn;
+            pq = pn;
+            pn = pn.next;
+        }
+        return dummy.next;
+    }
+}
+
+// other solution using recursion o(n) space becouse stack
+
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if ((head == null) || (head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
