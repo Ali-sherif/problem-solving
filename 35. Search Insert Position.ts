a7f9b6441bc72416d5500a3b3ad1fc75fcbd8588
Ali sherif <a.sheriffouad@gmail.com>
2026-08-10 19:57:40 +0300

    Add 35. Search Insert Position

---
+++ 35. Search Insert Position.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+        const mid = Math.floor(l + (r - l) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+
+    return l;
+}
